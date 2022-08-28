58892ef71c0b6cb9a2657f2bd75f58907ae3c29e
johanne <johanne_riis@hotmail.com>
2022-08-28 19:53:54 +0200

    first version, missing lots of details

---
+++ src/test/java/com/example/spaceportal/SpacePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spaceportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
