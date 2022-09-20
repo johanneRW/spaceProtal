cdd32ad7bb7a767437abc15a4ca1ea3f563702ba
johanne <johanne_riis@hotmail.com>
2022-09-20 23:17:38 +0200

    ændret opbygning

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -1,3 +1,39 @@
+//skjul og vis foskellige sider
+$(document).ready(function(){
+    loadPlanets();
+
+    $(".spaceships a").click(function(){
+        loadSpaceships();
+        $(".planetPage").addClass("d-none")
+        $(".spaceshipPage").removeClass("d-none")
+        $(".issPage").addClass("d-none")
+    });
+
+    $(".planets a").click(function(){
+        loadPlanets();
+        $(".spaceshipPage").addClass("d-none")
+        $(".planetPage").removeClass("d-none")
+        $(".issPage").addClass("d-none")
+    });
+
+    $(".iss a").click(function(){
+        loadISS();
+        $(".spaceshipPage").addClass("d-none")
+        $(".planetPage").addClass("d-none")
+        $(".issPage").removeClass("d-none")
+
+        //var plaque = $('.plaque');
+    });
+
+    $(".shiny-button#apod").click(function(){
+        $('#exampleModal').modal('toggle')
+        loadAPOD()
+    });
+
+
+});
+
+// Plantes
 var loadPlanets = function() {
     fetch(
         'http://localhost:8080/json/planets.json',
@@ -6,7 +42,6 @@ var loadPlanets = function() {
         response.json().then(planets => {
             updatePlanet(planets, 0);
             updatePlanetGrid(planets);
-            updatePlanetFacts(planets);
         });
     });
 }
@@ -38,46 +73,131 @@ var updatePlanetGrid = function(planets) {
     var container = $('.detail .grid-container');
 
     container.html('');
+    container.addClass('planet')
+        container.removeClass('iss')
+        container.removeClass('spaceship')
 
     for (let i = 0; i < planets.length; i++) {
         var imageUrl = planets[i]['imageUrl'];
         var name = planets[i]['name'];
-        var options = $('<div class="grid-item"><img class="responsive" data-id="' + i + '" src="' + imageUrl + '" /><p>' + name + '</p></div>');
+        var options = $('<a href="#" data-id="' + i + '"><div class="grid-item"><img class="responsive" src="' + imageUrl + '" /><p>' + name + '</p></div></a>');
         container.append(options);
     }
 
-    $('img', container).click(function () {
+    $('a', container).click(function () {
         var id = $(this).data('id');
         updatePlanet(planets, id);
     });
 };
 
-$(document).ready(function(){
-    loadPlanets();
 
-    $(".spaceships a").click(function(){
-        // $(".planetPage").addClass("d-none")
-        // $(".spaceshipPage").removeClass("d-none")
-        // $(".issPage").addClass("d-none")
+// Spaceships
+    var loadSpaceships = function() {
+        fetch(
+            'http://localhost:8080/json/spaceships.json',
+            { method: 'GET' }
+        ).then(response => {
+            response.json().then(spaceships => {
+                updateSpaceship(spaceships, 0);
+                updateSpaceshipGrid(spaceships);
+                
+            });
+        });
+    }
+    
+    var updateSpaceship = function(spaceships, spaceshipId) {
+        var spaceship = spaceships[spaceshipId];
+    
+        $('.bigFrameContent img').attr('src', spaceship['imageUrl']);
+    
+        $('.plaque').html('<div class="text"><h2></h2><p></p></div>');  // reset 'plaque' element
+        $('.plaque .text p').text(spaceship['notes']);
+        $('.plaque .text h2').text(spaceship['name']);
+    
+    
+        $("#spaceship-max-passengers").text(spaceship['maxPassengers']);
+        $("#spaceship-max-speed").text(spaceship['maxSpeed']);
+        $("#spaceship-max-load").text(spaceship['maxLoad']);
+        $("#spaceship-builddate").text(spaceship['buildDate']);
+        $("#spaceship-name").text(spaceship['name']);
+              
+        
+    };
+    
+    var updateSpaceshipGrid = function(spaceships) {
+        var container = $('.detail .grid-container');
+    
+        container.html('');
+        container.removeClass('planets')
+        container.removeClass('iss')
+        container.addClass('spaceship')
+    
+        for (let i = 0; i < spaceships.length; i++) {
+            var imageUrl = spaceships[i]['imageUrl'];
+            var name = spaceships[i]['name'];
+            var options = $('<a href="#" data-id="' + i + '"><div class="grid-item"><img class="responsive" src="' + imageUrl + '" /><p>' + name + '</p></div></a>');
+            container.append(options);
+        }
+    
+        $('a', container).click(function () {
+            var id = $(this).data('id');
+            updateSpaceship(spaceships, id);
+        });
+    };
+
+    // ISS
+    var loadISS = function() {
+        fetch(
+            'https://api.wheretheiss.at/v1/satellites/25544',
+            { method: 'GET' }
+        ).then(response => {
+            response.json().then(iss => {
+                updateISS(iss);
+                updateISSGrid(iss);
+                
+            });
+        });
+    }
+    
+    var updateISS = function(ISS) {
+        
+        $('.bigFrameContent img').attr('src',"/images/iss.png");
+    
+    
+        // $("#spaceship-max-passengers").text(spaceship['maxPassengers']);
+        // $("#spaceship-max-speed").text(spaceship['maxSpeed']);
+        // $("#spaceship-max-load").text(spaceship['maxLoad']);
+        // $("#spaceship-builddate").text(spaceship['buildDate']);
+        // $("#spaceship-name").text(spaceship['name']);
+              
+        
+    };
+    
+    var updateISSGrid = function(spaceships) {
+        var container = $('.detail .grid-container');
+
+        container.html('');
+        container.removeClass('planets')
+        container.addClass('iss')
+        container.removeClass('spaceship')
+    
+        container.append('<img src="/images/iss.png">')
+    
+    };
+
+    var loadAPOD=function(){ 
+        fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=0xBWwWrQ3fosBO3mfognfipbqRDMeWUQb40DxwcS',
+        { method: 'GET' }
+    ).then(response => {
+        response.json().then(APOD => {
+           var imageUrl=APOD['url']
+           var title=APOD['title']
+            var img=$('.modal img')
+            img.attr('src',imageUrl)
+            var imgTitle=$('.modal h5')
+            imgTitle.text(title)
+        });
     });
 
-    $(".planets a").click(function(){
-        loadPlanets();
-        // $(".spaceshipPage").addClass("d-none")
-        // $(".planetPage").removeClass("d-none")
-        // $(".issPage").addClass("d-none")
-    });
-
-    $(".iss a").click(function(){
-        var plaque = $('.plaque');
-        var btnApod = $('<div><a class="shiny-button"></a><p>APOD</p></div>');
-        var btnSpaceX = $('<div><a class="shiny-button"></a><p>SpaceX Launches</p></div>');
-        plaque.html('');
-        plaque.append(btnApod);
-        plaque.append(btnSpaceX);
-
-        // $(".spaceshipPage").addClass("d-none")
-        // $(".planetPage").addClass("d-none")
-        // $(".issPage").removeClass("d-none")
-    });
-});
+    }
